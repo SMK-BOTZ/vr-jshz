6bf2d32cf81496a3c9f8154e74074be97dad0a43
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-11-28 14:54:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://web-omh0zp1b6v6k.up-de-fra1-k8s-1.apps.run-on-seenode.com//")
+URL = environ.get("FQDN", "https://web-omh0zp1b6v6k.up-de-fra1-k8s-1.apps.run-on-seenode.com/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
